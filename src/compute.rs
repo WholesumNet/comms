eeb10ed7ea8055b18ce2a0464c6cce58e26940e0
rezahsnz <rezahsnz@proton.me>
2024-12-17 19:48:22 +0330

    add: new gossip & match mechanism for zeth proving

--- src/compute.rs
+++ src/compute.rs
@@ -1,80 +1,68 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub enum ComputeType {
-    // the compute is prove and lift
-    ProveAndLift,
-    
-    // the compute is join
-    Join,
 
-    // the compute is snark extraction(r0 identity_p254 + compress)
-    Snark,
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct ProveDetails {
+
+    // a typical segment's cid: bafybeiccjcinml5w2meuhcnzu7gwlbkioy2dtyskulrspxoys6gikrrzae/segment-0
+
+    // all segments are in a directory pointed to by the base cid
+    pub segments_base_cid: String,
+
+    // "segment-" in the ^ example 
+    pub segment_prefix_str: String,
+
+    // po2 limit of the segment, e.g. 19 requires 4gb memory to prove
+    pub po2: u8,
+
+    // number of segments, used by provers to access any segment(0 to num_segments - 1)
+    pub num_segments: u32,
 }
 
-// criteria for job seekers
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Criteria {
-    // the actual type compute needed
-    pub compute_type: ComputeType,
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct JoinDetails {
+
+    // a list of (left, right) tuples, order is important when reporting the result
+    pub pairs: Vec<(String, String)>,    
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct SnarkDetails {
+
+    // stark proof's cid
+    pub cid: String,    
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct VerificationDetails {
+
+    // SuccinctReceipt's cid
+    pub cid: String,    
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum ComputeType {
+
+    ProveAndLift(ProveDetails),
+    
+    Join(JoinDetails),
+
+    Snark(SnarkDetails),
+
+    // reserved for future
+    Verification(VerificationDetails),
 }
 
 // used by clients when gossiping about compute needs
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct NeedCompute {
-    pub criteria: Criteria,
-}
-
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct ServerSpecs {
-    pub gflops: u16,            //@ fp64 * 10,000?
-    pub memory_capacity: u32,   // in GB
-    pub cpu_model: String,
-}
-
-// the offer as server makes for a compute need
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Offer {
-    pub compute_type: ComputeType,
-    pub hw_specs: ServerSpecs,                  
-    pub price: u8,                              
-}
-
-
-// input data for the job
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub enum ComputeJobInputType {
-    // cid of the segment to be proved and lifted
-    Prove(String),
-
-    // cid of the left and right succinct receipts
-    Join(String, String),
-}
-
-// the actual compute job 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct ComputeDetails {
+    // network-wide id of the job
     pub job_id: String,
-    
-    pub compute_type: ComputeType,
-    
-    pub input_type: ComputeJobInputType,
-}
 
-// 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct VerificationDetails {
-    pub job_id: String,
-    pub risc0_image_id: String,       // image_id(merkle root of) as in Risc0    
-    pub receipt_cid: String,          // receipt to verify against as in Risc0
-    //@ no need for pod_name here, could be hard-coded to 'receipt'
-    pub pod_name: String,             // pod where the receipt is located in FairOS-dfs
-}
+    // whether it's prove, join, or snark
+    pub compute_type: ComputeType,    
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct HarvestDetails {
-    pub fd12_cid: Option<String>,
-    pub receipt_cid: Option<String>,
+    pub budget: u32,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -86,17 +74,30 @@ pub enum JobStatus {
     ExecutionFailed(Option<String>),    
 
     // waiting to be verified, receipt cid as param
-    ExecutionSucceeded(String),  
+    ExecutionSucceeded(String), 
+}
 
-    // harvested, cid of fd12, logs, ... as params
-    Harvested(HarvestDetails),  
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum ItemType {
+
+    Prove,
+
+    Join,
+
+    Snark, 
+
+    Verification,
 }
 
 // servers update clients about latest developments of jobs 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct JobUpdate {
+
     pub id: String,
-    pub compute_type: ComputeType,
+
+    // type of the item being reported
+    pub item_type: ItemType,
+
     pub status: JobStatus,
 }
 
